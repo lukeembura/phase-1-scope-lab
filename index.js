89ecc8e3ff3e2908837e4aad228c1d331b6cf893
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-03-05 14:25:03 +0300

    Adding files

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Declare customerName in the global scope
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // This will throw an error
+}
